e7000922fed5a6fe3342a4f5fdc8c27ab0e93329
JingQian87 <qianjing8758@gmail.com>
2019-04-19 00:08:28 -0400

    generate graph
    error happens when import shortest path

---
+++ HW5/hw5/shortest_path.py
@@ -0,0 +1,100 @@
+from collections import defaultdict
+from heapq import heapify, heappush, heappop
+import matplotlib.pyplot as plt
+
+#from gene_vgraph import gene_vgraph
+
+def manhattan_dist(p1, p2):
+    """ Returns the manhattan distance between two points """
+    return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** .5
+
+def dijkstra(start, goal, adj_list):
+    visited, dist, prev, heap = set(), dict(), dict(), []
+    for vertex in adj_list.keys():
+        if vertex == start:
+            dist[vertex] = 0
+            prev[vertex] = None
+            heap.append((0, vertex))
+        else:
+            dist[vertex] = float('inf')
+            prev[vertex] = None
+            heap.append((float('inf'), vertex))
+    heapify(heap)
+
+    while heap:
+        _, vertex = heappop(heap)
+        if vertex in visited:
+            continue
+        elif vertex == goal: # found min distance to goal, no need to continue
+            break
+        visited.add(vertex)
+        for neighbor, edge_len in adj_list[vertex]:
+            if neighbor in visited:
+                continue
+            elif dist[vertex] + edge_len < dist[neighbor]:
+                dist[neighbor] = dist[vertex] + edge_len
+                prev[neighbor] = vertex
+                heappush(heap, (dist[neighbor], neighbor))
+    
+    # Recover the path
+    path, curr = [], goal
+    while curr:
+        path.append(curr)
+        curr = prev[curr]
+    
+    return path[::-1], dist[goal]
+
+def shortest_path(obstacles_file, goal_file):
+    """ Return the shortest path from start to goal """
+    # edges = [((1, 2), (3, 4)), ((3, 4), (2, 4)), ((1, 2), (5, 6)), ((2, 4), (5, 6))]
+    # start, goal = (1, 2), (5, 6)
+
+    edges, goal = gene_vgraph(obstacles_file, goal_file)
+    start = (0, 0)
+    
+    # Compute adjacency list
+    adj_list = defaultdict(list)
+    for edge in edges:
+        edge_len = manhattan_dist(edge[0], edge[1])
+        adj_list[edge[0]].append((edge[1], edge_len))
+        adj_list[edge[1]].append((edge[0], edge_len))
+    # print(adj_list)
+    
+    return dijkstra(start, goal, adj_list)
+
+def main():
+    obstacles_file = "../data/world_obstacles.txt"
+    goal_file = "../data/goal.txt"
+    path_file = "../data/path.txt"
+
+    # Plotting purposes only
+    from grow_obstacles import grow_obstacle, plot_obstacle
+    from utils import read_world_data
+
+    obstacles, _ = read_world_data(obstacles_file, goal_file)
+    
+    for obstacle in obstacles:
+        plot_obstacle(obstacle)
+        obstacle_new = grow_obstacle(obstacle)
+        plot_obstacle(obstacle_new)
+
+    edges, _ = gene_vgraph(obstacles_file, goal_file)
+    for i in edges:
+        xs = [i[0][0], i[1][0]]
+        ys = [i[0][1], i[1][1]]
+        plt.plot(xs, ys, 'b', lw=1)
+
+    points, path_len = shortest_path(obstacles_file, goal_file)
+    xs = [point[0] for point in points]
+    ys = [point[1] for point in points]
+    plt.plot(xs, ys, 'g--', lw=3)
+    # plt.show()
+
+    lines = []
+    for point in points:
+        lines.append("{} {}\n".format(point[0], point[1]))
+    with open(path_file, 'w') as f:
+        f.writelines(lines)
+
+if __name__ == "__main__":
+    main()
--- HW5/hw5/vanillaPRM.py
+++ HW5/hw5/vanillaPRM.py
@@ -13,11 +13,12 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 
 from visualize_map import *
+from shortest_path import *
 
 obstacle_file = 'world_obstacles.txt'
 start_goal_file = 'start_goal.txt'
 
-def generate_node(path_data, nsample):
+def generate_nodes(path_data, nsample):
 	V = []
 	# sample uniformly until have nsample collision-free nodes.
 	xy_min = [0, 0]
@@ -28,6 +29,25 @@ def generate_node(path_data, nsample):
 			V.append(q)
 	return V
 
+def edges_obstacles(path_data):
+	E_obstacles = []
+	for i in range(1, len(path_data)-1):
+		if path_data.codes[i] == 1: 
+			start = path_data.vertices[i]
+		elif path_data.codes[i] == 79:
+			continue
+		elif path_data.codes[i+1] == 79:
+			E_obstacles.append((path_data.vertices[i], start))
+			continue
+		E_obstacles.append((path_data.vertices[i], path_data.vertices[i+1]))
+	return np.array(E_obstacles)
+
+def ccw(A,B,C):
+	return (C[1]-A[1])*(B[0]-A[0]) > (B[1]-A[1])*(C[0]-A[0])
+
+def intersect(A,B,C,D):
+	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
 def generate_edges(path_data, nodes, k):
 	""" Find k nearest neighbors for each node.
 		In the sklearn.neighbors.NearestNeighbors, k=1 returns the point itself.
@@ -37,25 +57,51 @@ def generate_edges(path_data, nodes, k):
 	nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='brute').fit(nodes)
 	distances, indices = nbrs.kneighbors(nodes)
 	#print(distances, indices)
-	for i in range(nsample):
-		q = V[i]
+
+	E_obs = edges_obstacles(path_data)
+	E = []
+
+	for i in range(len(nodes)):
+		q = tuple(nodes[i].tolist())
 		kindices = indices[i, 1:]
 		#print(q, knbrs)
 		for j in kindices:
-			qj = V[j]
-			if (q, qj) not in E and no collision detected:
-				E.append((q, qj))
+			qj = tuple(nodes[j].tolist())
+			if (q, qj) not in E and (q, qj) not in E:
+				for k in E_obs:
+					if intersect(q, qj, k[0], k[1]):
+						break
+				else:
+					E.append((q, qj))
 	return E
 
+def new_spath(start, goal, edges):
+	# Compute adjacency list
+	adj_list = defaultdict(list)
+	for edge in edges:
+		edge_len = manhattan_dist(edge[0], edge[1])
+		adj_list[edge[0]].append((edge[1], edge_len))
+		adj_list[edge[1]].append((edge[0], edge_len))
+	# print(adj_list)
 
-#def shortest path():
-"""Import from last hw?"""
+	return dijkstra(start, goal, adj_list)
+
 
 if __name__ == "__main__":
 	fig, ax = plt.subplots()
 	path = build_obstacle_course(obstacle_file, ax)
 	start, goal = add_start_and_goal(start_goal_file, ax)
-	point = (200,250)
-	a = path.contains_point(point)
-	V = generate_graph(path, 5, 2)
+	V = generate_nodes(path, 10)
+	E = generate_edges(path, V, 5)
+	for e in E:
+		ep = Path(e)
+		epatch = patches.PathPatch(ep, facecolor='None', edgecolor='xkcd:blue')
+		ax.add_patch(epatch)
+
+	points, path_len = new_spath(start, goal, E)
+	xs = [point[0] for point in points]
+	ys = [point[1] for point in points]
+	plt.plot(xs, ys, 'g--', lw=3)
+
+	plt.show()
 
