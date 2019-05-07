b5a45249c4b8aa79f86f564ca04d9256a0d7b166
JingQian87 <qianjing8758@gmail.com>
2019-05-07 01:12:35 -0400

    done 1

--- HW6/release/robot.py
+++ HW6/release/robot.py
@@ -1,4 +1,6 @@
 """
+    Notice!!! Jing finished Q1 & Q2. In Q1, Jing only consider that the center 
+
     Take the simple_particle_pka.py from Professor Peter Allen's notes as reference.
     Note:
     1. Landmarks are squares centered in (x,y) with landmark_size.
@@ -12,6 +14,12 @@ from visualize import *
 
 landmark_size = 6 #If change this, should change that in visualize.py too.
 filename = './world.txt'
+
+def ccw(A,B,C):
+    return (C[1]-A[1])*(B[0]-A[0]) > (B[1]-A[1])*(C[0]-A[0])
+
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
 class Robot:
 
@@ -38,27 +46,49 @@ class Robot:
         self.bearing_noise = float(new_b_noise)#uncertainty in angle
 
 
-    # TODO: Moves a robot
-    def move(self, turn_angle):
+    # Done: Moves a robot
+    def move(self, angle_step = 0.1):
         """
         This function is to update the robot's state according to nonlinear transition function and return the action uk = [10cos(theta_k), 10sin(theta_k), delta_k]
-        The order of move: move angle delta_k first, then translation.
-        Before retun, call sense to determine movable; if not, turn angle until save to move forward, delta_k = total turning angle.
-        Consider noise in transition and observation????
+        The order of move: move angle with angle_step first, then translation.
+        Before retun, call sense to determine movable; if not, turn angle until safe to move forward, delta_k = total turning angle.
         """
         self.set_state(self.x, self.y, self.theta)
-        forward = 10
+        forward = 10.0
 
+        obstacles = self.sense()
+        sensed_landmarks = obstacles[1:]
 
-        return (0,0,0)
+        niter = 0
+        theta_update = self.theta + angle_step + random.gauss(0.0, self.turn_noise) #radius
+        dist = 10.0 + random.gauss(0.0, self.forward_noise)
+        flag = True
+        while flag and niter < 10000:
+            niter += 1
+            x_update = self.x + cos(theta_update) * dist
+            y_update = self.y + sin(theta_update) * dist
+            # check wall
+            if x_update > 0 and x_update < obstacles[0][0]:
+                if y_update > 0 and y_update < obstacles[0][1]:
+                    # check landmarks
+                    for i in range(len(obstacles)-1):
+                        imark = sensed_landmarks[i]
+                        if imark[0] > dist or imark[0]*abs(theta_update - imark[1]) > 1.42 * landmark_size * 0.5: 
+                            continue # no collision
+                        else:
+                            break # collision
+                    else:
+                        flag = False
+            theta_update += angle_step
+        return (x_update, y_update, theta_update)
 
-    # TODO: Returns a list of range bearing measurements
+    # Done: Returns a list of range bearing measurements
     def sense(self):
     """
     The Gaussian noise random variables are all zero-mean and have variance given by range noise for the range measurements and bearing noise for the bearing measurements, respectively (means univariate Gaussian).
     """
-        Z = []
         world_size, landmarks = read_txt(filename)
+        Z = [world_size]
         for i in range(len(landmarks)):
             dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
             dist += random.gauss(0.0, self.range_noise)
